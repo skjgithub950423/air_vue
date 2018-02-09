674ad09fbc0d9f20e60e68125af7deeca91a0eea
skjgithub950423 <286303352@qq.com>
2018-02-09 08:07:05 +0800

    拯救之前手欠覆盖掉的提交

---
+++ src/components/common/AirTable.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-table 
+  :data="tableData" 
+  :stripe="defaultConfig.isStripe"
+  :border="defaultConfig.isBorder"
+  :height="defaultConfig.tableHeight"
+  :columData="columData"
+  @row-click="handleSelect"
+  >
+        <el-table-column
+      :type="defaultConfig.isType"
+      width="55"
+      align="center"
+      :fixed="defaultConfig.isFixed">
+    </el-table-column>
+        <el-table-column v-for="col in columData" :prop="col.key" :label="col.name" :sortable="col.isSortable" :fixed="col.isFixed" :width="col.width">
+        </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name:'AirTable',
+  props:{
+      defaultConfig:{
+          required:true,
+          type:Object
+      },
+      tableData:{
+          type:Object
+      },
+      columData:{
+          required:true,
+          type:Object
+      },
+      rowClick:{
+          type:Object
+      }
+  },
+  methods:{
+      handleSelect(fn){
+          this.rowClick(fn)
+      },
+  }
+}
+</script>
+
--- src/pages/Tables/Table.vue
+++ src/pages/Tables/Table.vue
@@ -1,3 +1,70 @@
 <template>
-    <router-view></router-view>
+<div>
+<router-view></router-view>
+<AirTable 
+:defaultConfig="defaultConfig" 
+:tableData="tableData"
+:columData="columData"
+:rowClick="rowClick"></AirTable>
+</div>
 </template>
+
+<script>
+import AirTable from '../../components/common/AirTable'
+export default {
+  components: {AirTable},
+  data(){
+    return{
+      defaultConfig:{
+        isStripe:true,
+        isBorder:true,
+        tableHeight:'auto',
+        isType:'selection',
+        isFixed:true
+      },
+      tableData:[{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄',
+            number: '1111111'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄',
+            number: '1111111'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄',
+            number: '1111111'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄',
+            number: '1111111'
+          }],
+      columData:[{
+        name:'日期',
+        key:'date',
+        isSortable:true,
+        isFixed:true,
+        width:'300'
+      },{
+        name:'姓名',
+        key:'name'
+      },{
+        name:'地址',
+        key:'address'
+      },{
+        name:'编号',
+        key:'number'
+      }],
+    }
+  },
+  methods:{
+        rowClick:function(fn){
+          console.log(fn);
+        }
+      }
+}
+</script>
